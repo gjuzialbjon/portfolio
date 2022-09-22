7fc02e6253a02acd6b392ca39315ea97ef7ceff8
gjuzialbjon <gjuzialbjon@gmail.com>
2022-09-21 22:08:22 -0700

    update cv

--- src/app/views/cv/cv.module.ts
+++ src/app/views/cv/cv.module.ts
@@ -5,10 +5,9 @@ import { CvRoutingModule } from './cv-routing.module'
 import { CvWrapperComponent } from './components/cv-wrapper/cv-wrapper.component'
 import { HttpClientModule } from '@angular/common/http'
 import { TranslateModule } from '@ngx-translate/core'
-import { ProgressBarModule } from 'angular-progress-bar'
 
 @NgModule({
   declarations: [CvWrapperComponent],
-  imports: [CommonModule, CvRoutingModule, ProgressBarModule, HttpClientModule, TranslateModule.forChild()],
+  imports: [CommonModule, CvRoutingModule, HttpClientModule, TranslateModule.forChild()],
 })
 export class CvModule {}
